0d0f5af709a2bf7b433034c82dbbbd665131e7e0
Aharon Petrosyan <petrosyan-ahar@mail.ru>
2025-09-13 11:09:06 +0400

    add

--- ft_philo_start_day.c
+++ ft_philo_start_day.c
@@ -24,6 +24,8 @@ void	ft_sleep(t_args *st, int id)
 	ft_get_my_time(&st->t_end);
 	st->tt = (st->t_end.tv_sec - st->t_start.tv_sec) * 1000
              + (st->t_end.tv_usec - st->t_start.tv_usec) / 1000;
+	if (st->tt > st->time_to_die)
+		st->someone_died = 0;
 	st->time += st->tt;
 	pthread_mutex_lock(&st->print_mutex);
 	printf("%ld %d is sleeping\n",st->time, id);
@@ -52,6 +54,8 @@ void	ft_eat(t_args *st, int left, int rigth, int id)
 	st->tt = (st->t_end.tv_sec - st->t_start.tv_sec) * 1000
              + (st->t_end.tv_usec - st->t_start.tv_usec) / 1000;
 	st->time += st->tt;
+	if (st->tt > st->time_to_die)
+		st->someone_died = 0;
 	pthread_mutex_lock(&st->print_mutex);
 	printf("%ld %d has taken a fork\n", st->time, id);
 	printf("%ld %d has taken a fork\n", st->time, id);
@@ -64,6 +68,8 @@ void	ft_eat(t_args *st, int left, int rigth, int id)
 
 int	ft_get_fork(t_philo *philo, int id)
 {
+	if (st->tt > st->time_to_die)
+		st->someone_died = 0;
 	if (id % 2 != 0)
 	{
 		ft_eat(philo->st, philo->st->philo[id].left_fork,
